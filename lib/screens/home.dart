b19ea7985febaa5364e3cc8469139c4fda486f68
FatumaA <67555014+FatumaA@users.noreply.github.com>
2021-12-24 02:25:13 +0300

    protect home route

---
+++ lib/screens/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:supa_auth_flutter/utils/supabase.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  User? activeUser = SupabaseHelper().getActiveUser();
+
+  @override
+  Widget build(BuildContext context) {
+    if (activeUser == null) {
+      Navigator.pop(context, '/');
+    }
+    return Center(
+      child: Column(
+        children: [
+          const Text(
+            'Karibu! You are logged In successfully',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          const SizedBox(
+            height: 100,
+          ),
+          TextButton(
+            onPressed: () {
+              SupabaseHelper().signOutActiveUser();
+              Navigator.pop(context, '/');
+            },
+            child: const Text(
+              'Sign Out',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
