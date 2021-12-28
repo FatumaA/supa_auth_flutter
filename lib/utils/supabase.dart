3707b0d34f1c1d6741b692b2c00632212258b467
FatumaA <67555014+FatumaA@users.noreply.github.com>
2021-12-28 22:32:02 +0300

    clean up the supabase file

--- lib/utils/supabase.dart
+++ lib/utils/supabase.dart
@@ -1,6 +1,7 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseHelper {
+  final supaClient = Supabase.instance.client;
   // email-password sign up
   Future<GotrueSessionResponse> createNewUser(
       String email, String password) async {
@@ -12,15 +13,14 @@ class SupabaseHelper {
   // phone auth with password and verification step
   Future<GotrueSessionResponse> createNewPhoneUser(
       String phone, String password) async {
-    final res =
-        await Supabase.instance.client.auth.signUpWithPhone(phone, password);
+    final res = await supaClient.auth.signUpWithPhone(phone, password);
     // signUpWithPhone(phone, password);
     return res;
   }
 
   Future<GotrueSessionResponse> verifyPhoneUser(
       String phone, String token) async {
-    final res = await Supabase.instance.client.auth.verifyOTP(
+    final res = await supaClient.auth.verifyOTP(
       phone,
       token,
       options: AuthOptions(redirectTo: 'http://localhost:53463/home'),
@@ -31,8 +31,10 @@ class SupabaseHelper {
 
   Future<GotrueSessionResponse> signInUserWithPhone(
       String phone, String password) async {
-    final res = await Supabase.instance.client.auth
-        .signIn(phone: phone, password: password);
+    final res = await supaClient.auth.signIn(
+      phone: phone,
+      password: password,
+    );
     // .signIn(phone: phone, password: password);
     print(res.error);
     return res;
@@ -41,7 +43,7 @@ class SupabaseHelper {
   // email-password sign in
   Future<GotrueSessionResponse> signInExistingUser(
       String email, String password) async {
-    final res = await Supabase.instance.client.auth.signIn(
+    final res = await supaClient.auth.signIn(
       email: email,
       password: password,
     );
@@ -51,7 +53,7 @@ class SupabaseHelper {
 
   // email magic link sign in
   Future<GotrueSessionResponse> createNewPasswordlessUser(String email) async {
-    final res = await Supabase.instance.client.auth.signIn(
+    final res = await supaClient.auth.signIn(
       email: email,
       options: AuthOptions(redirectTo: 'http://localhost:53463/home'),
     );
@@ -61,7 +63,7 @@ class SupabaseHelper {
 
   // social login with Google
   Future<bool> signInWithGoogle() async {
-    final res = await Supabase.instance.client.auth.signInWithProvider(
+    final res = await supaClient.auth.signInWithProvider(
       Provider.google,
       options: AuthOptions(redirectTo: 'http://localhost:53463/home'),
     );
@@ -75,7 +77,7 @@ class SupabaseHelper {
 
   // sign out active user
   Future<GotrueResponse> signOutActiveUser() async {
-    final res = await Supabase.instance.client.auth.signOut();
+    final res = await supaClient.auth.signOut();
 
     return res;
   }
@@ -83,7 +85,7 @@ class SupabaseHelper {
   // sends user a reset password email, redirectTo - screen user comes back to
   Future<GotrueJsonResponse> resetExistingUserPassword(
       String email, String? urlPath) async {
-    final res = await Supabase.instance.client.auth.api.resetPasswordForEmail(
+    final res = await supaClient.auth.api.resetPasswordForEmail(
       email,
       options: AuthOptions(
         redirectTo: urlPath,
@@ -93,18 +95,23 @@ class SupabaseHelper {
     return res;
   }
 
-  Future<GotrueUserResponse> updateUserPassword(String accessToken, String password) async {
-    final res = await Supabase.instance.client.auth.api.updateUser(
+  Future<GotrueUserResponse> updateUserPassword(
+      String accessToken, String password) async {
+    final res = await supaClient.auth.api.updateUser(
       accessToken,
-      UserAttributes(password: ''),
+      UserAttributes(password: password),
     );
+
+    //  final res = await Supabase.instance.client.auth.update(
+    //     UserAttributes(data: {'password': password})
+    // );
 
     return res;
   }
 
   // get active user
   User? getActiveUser() {
-    final user = Supabase.instance.client.auth.currentUser;
+    final user = supaClient.auth.currentUser;
     print('user: $user');
 
     return user;
