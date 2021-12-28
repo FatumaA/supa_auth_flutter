922e5379fc6054df44848f87a29932b16807d723
FatumaA <67555014+FatumaA@users.noreply.github.com>
2021-12-28 08:46:14 +0300

    updates to supabase util file

--- lib/utils/supabase.dart
+++ lib/utils/supabase.dart
@@ -2,7 +2,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseHelper {
   // email-password sign up
-  Future<GotrueResponse> createNewUser(String email, String password) async {
+  Future<GotrueSessionResponse> createNewUser(
+      String email, String password) async {
     final res = await Supabase.instance.client.auth.signUp(email, password);
 
     return res;
@@ -12,27 +13,33 @@ class SupabaseHelper {
   Future<GotrueSessionResponse> createNewPhoneUser(
       String phone, String password) async {
     final res =
-        await Supabase.instance.client.auth.signUpWithPhone(phone, password); 
-        // signUpWithPhone(phone, password);
+        await Supabase.instance.client.auth.signUpWithPhone(phone, password);
+    // signUpWithPhone(phone, password);
     return res;
   }
 
-  Future<GotrueSessionResponse> verifyPhoneUser(String phone, String token) async {
-    final res = await Supabase.instance.client.auth.verifyOTP(phone, token);
+  Future<GotrueSessionResponse> verifyPhoneUser(
+      String phone, String token) async {
+    final res = await Supabase.instance.client.auth.verifyOTP(
+      phone,
+      token,
+      options: AuthOptions(redirectTo: 'http://localhost:53463/home'),
+    );
 
     return res;
   }
 
   Future<GotrueSessionResponse> signInUserWithPhone(
       String phone, String password) async {
-    final res = await Supabase.instance.client.auth.signIn(phone: phone, password: password);
-        // .signIn(phone: phone, password: password);
+    final res = await Supabase.instance.client.auth
+        .signIn(phone: phone, password: password);
+    // .signIn(phone: phone, password: password);
     print(res.error);
     return res;
   }
 
   // email-password sign in
-  Future<GotrueResponse> signInExistingUser(
+  Future<GotrueSessionResponse> signInExistingUser(
       String email, String password) async {
     final res = await Supabase.instance.client.auth.signIn(
       email: email,
@@ -43,8 +50,11 @@ class SupabaseHelper {
   }
 
   // email magic link sign in
-  Future<GotrueResponse> createNewPasswordlessUser(String email) async {
-    final res = await Supabase.instance.client.auth.signIn(email: email);
+  Future<GotrueSessionResponse> createNewPasswordlessUser(String email) async {
+    final res = await Supabase.instance.client.auth.signIn(
+      email: email,
+      options: AuthOptions(redirectTo: 'http://localhost:53463/home'),
+    );
 
     return res;
   }
@@ -53,8 +63,12 @@ class SupabaseHelper {
   Future<bool> signInWithGoogle() async {
     final res = await Supabase.instance.client.auth.signInWithProvider(
       Provider.google,
-      options: AuthOptions(redirectTo: 'http://localhost:53463/#/home/'),
+      options: AuthOptions(redirectTo: 'http://localhost:53463/home'),
     );
+    // signInWithProvider(
+    //   Provider.google,
+    //   options: AuthOptions(redirectTo: 'http://localhost:53463/home'),
+    // );
 
     return res;
   }
@@ -67,7 +81,7 @@ class SupabaseHelper {
   }
 
   // sends user a reset password email, redirectTo - screen user comes back to
-  Future<GotrueResponse> resetExistingUserPassword(
+  Future<GotrueJsonResponse> resetExistingUserPassword(
       String email, String? urlPath) async {
     final res = await Supabase.instance.client.auth.api.resetPasswordForEmail(
       email,
@@ -79,9 +93,19 @@ class SupabaseHelper {
     return res;
   }
 
+  Future<GotrueUserResponse> updateUserPassword(String accessToken, String password) async {
+    final res = await Supabase.instance.client.auth.api.updateUser(
+      accessToken,
+      UserAttributes(password: ''),
+    );
+
+    return res;
+  }
+
   // get active user
   User? getActiveUser() {
     final user = Supabase.instance.client.auth.currentUser;
+    print('user: $user');
 
     return user;
   }
